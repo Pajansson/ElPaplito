40315fdaa1bddf5493981c75a1f04d124c340787
Patrik Jansson <Patrik_jansson@hotmail.com>
2017-11-01 10:51:11 +0100

    Add class Tranaction, Customer and Account

---
+++ BankLib/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLib
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int FromAccountId { get; set; }
+        public int ToAccountId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
