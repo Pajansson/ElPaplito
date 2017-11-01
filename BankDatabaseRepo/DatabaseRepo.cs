542339bfab7f12816125cc4d08bdc7242b7985a5
Andre <andre.holmlund@gmail.com>
2017-11-01 11:15:57 +0100

    Added collections ti DbRepo

--- BankDatabaseRepo/DatabaseRepo.cs
+++ BankDatabaseRepo/DatabaseRepo.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿using BankLib;
+using System;
+using System.Collections.Generic;
 
 namespace BankDatabaseRepo
 {
-    public class Class1
+
+    public class DatabaseRepo
     {
+
+        private List<Account> _accounts;
+        private List<Customer> _customer;
+        private List<Transaction> _transaction;
+
+        public DatabaseRepo()
+        {
+            _accounts = new List<Account>();
+            _customer = new List<Customer>();
+            _transaction = new List<Transaction>();
+
+        }
+
+
+
     }
 }
