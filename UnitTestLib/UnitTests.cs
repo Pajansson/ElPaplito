1aac9ee5a4475e483f15f3891e9849f89a1696e5
Patrik Jansson <Patrik_jansson@hotmail.com>
2017-11-07 21:20:30 +0100

    Added unittest for getCustomerAccount

--- UnitTestLib/UnitTests.cs
+++ UnitTestLib/UnitTests.cs
@@ -1,7 +1,9 @@
 using BankLib;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using BankLogicRepo;
 using Xunit;
 
 namespace UnitTestLib
@@ -109,6 +111,36 @@ namespace UnitTestLib
             string[] arr = System.IO.File.ReadAllLines($"C:\\Development\\Bank\\{x}");
 
             Assert.NotEqual(0, arr.Length);
+        }
+
+        [Fact]
+        public void GetCustomerAccount()
+        {
+            var bankLogic = new BankLogic();
+            var accounts = new List<Account>();
+            var acc1 = new Account
+            {
+                Balance = 1111,
+                AccountId = 1,
+                CustomerId = 1
+            };
+            var acc2 = new Account
+            {
+                Balance = 3333,
+                AccountId = 2,
+                CustomerId = 1
+            };
+            var acc3 = new Account
+            {
+                Balance = 444,
+                AccountId = 3,
+                CustomerId = 3
+            };
+            accounts.Add(acc1);
+            accounts.Add(acc2);
+            accounts.Add(acc3);
+            var result = bankLogic.GetCustomersAccounts(1, accounts).Count;
+            Assert.Equal(2, result);
         }
     }
 }
